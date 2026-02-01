1e47cbe4a89b3322bfd9a8d1a5f236b0f2fba43c
imtarcrow <github@tarcrow.com>
2026-02-01 10:36:39 +0100

    changed theme for neovim

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,72 +1,81 @@
 return {
-    -- Themes
-    {
-        'mellow-theme/mellow.nvim',
-        lazy = false,
-        prioriy = 1000,
-        config = function ()
-            vim.g.mellow_transparent = true
-            vim.g.mellow_bold_booleans = true
-            vim.g.mellow_italic_comments = true
-        end
-    },
-    {
-        'rebelot/kanagawa.nvim',
-        lazy = false,
-        priority = 1000,
-        opts = {},
-    },
-    {
-        'rose-pine/neovim',
-        name = 'rose-pine',
-        lazy = false,
-        priority = 1000,
-        opts = {},
-    },
-    {
-        'vague-theme/vague.nvim',
-        lazy = false,
-        priority = 1000,
-        opts = {
-            transparent = true,
-        },
-        config = function() 
-            vim.cmd([[colorscheme vague]])
-        end
-    },
+	-- Themes
+	{
+		"mellow-theme/mellow.nvim",
+		lazy = false,
+		prioriy = 1000,
+		config = function()
+			vim.g.mellow_transparent = true
+			vim.g.mellow_bold_booleans = true
+			vim.g.mellow_italic_comments = true
 
-    -- Lualine
-    {
-        'nvim-lualine/lualine.nvim',
-        lazy = false,
-        config = function ()
-            require('lualine').setup({
-                options = {
-                    icons_enabled = true,
-                    theme = 'seoul256',
-                    sections = {
-                        lualine_a = {'mode'},
-                        lualine_b = {'branch', 'diff', 'diagnostics'},
-                        lualine_c = {'filename', 'filesize'},
-                        lualine_x = {'lsp_status', 'encoding', 'fileformat', 'filetype'},
-                        lualine_y = {'progress'},
-                        lualine_z = {'location'},
-                    }
-                }
-            })
-        end
-    },
+			vim.cmd([[colorscheme mellow]])
 
-    -- Gitsigns
-    {
-        'lewis6991/gitsigns.nvim',
-        lazy = false,
-        opts = {
-            current_line_blame = true,
-            current_line_blame_opts = {
-                delay = 0,
-                virt_text_pos = 'right_align'
-            },
-        }
-    },
+		end,
+	},
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	{
+		"vague-theme/vague.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			transparent = true,
+		},
+		config = function()
+		end,
+	},
+
+	-- Lualine
+	{
+		"nvim-lualine/lualine.nvim",
+		lazy = false,
+		config = function()
+			require("lualine").setup({
+				options = {
+					icons_enabled = true,
+					theme = "seoul256",
+					sections = {
+						lualine_a = { "mode" },
+						lualine_b = { "branch", "diff", "diagnostics" },
+						lualine_c = { "filename", "filesize" },
+						lualine_x = { "lsp_status", "encoding", "fileformat", "filetype" },
+						lualine_y = { "progress" },
+						lualine_z = { "location" },
+					},
+				},
+			})
+		end,
+	},
+
+	-- Gitsigns
+	{
+		"lewis6991/gitsigns.nvim",
+		lazy = false,
+		opts = {
+			current_line_blame = true,
+			current_line_blame_opts = {
+				delay = 0,
+				virt_text_pos = "right_align",
+			},
+		},
+	},
 }
