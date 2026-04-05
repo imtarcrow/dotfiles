741adc4fd9511e2a54240f6bd0e51c0db149bfa8
imtarcrow <github@tarcrow.com>
2026-04-05 20:27:36 +0200

    made keymaps dependend on lspattach event

--- .config/nvim/lua/core/keymap.lua
+++ .config/nvim/lua/core/keymap.lua
@@ -1,18 +1,25 @@
 
-vim.keymap.set("n", "gd", vim.lsp.buf.definition)
-vim.keymap.set("n", "K", vim.lsp.buf.hover)
-vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol)
-vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float)
-vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action)
-vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references)
-vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename)
-vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help)
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("LspMappings", { clear = true}),
+    callback = function(args)
+        local bufnr = args.buf
 
-vim.keymap.set("n", "<leader>F", vim.lsp.buf.format)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer = bufnr})
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer = bufnr})
 
--- vim.keymap.set('n', '<C-d>', '<C-d>zz')
--- vim.keymap.set('n', '<C-u>', '<C-u>zz')
+        vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, {buffer = bufnr})
+        vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, {buffer = bufnr})
+        vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, {buffer = bufnr})
+        vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, {buffer = bufnr})
+        vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, {buffer = bufnr})
+
+        vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, {buffer = bufnr})
+        vim.keymap.set("n", "<leader>F", vim.lsp.buf.format, {buffer = bufnr})
+    end
+})
+
+
 
 vim.keymap.set("n", "<leader>th", function()
-	vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = 0 }))
+    vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = 0 }))
 end, { buffer = bufnr, desc = "[T]oggle Inlay [H]ints" })
