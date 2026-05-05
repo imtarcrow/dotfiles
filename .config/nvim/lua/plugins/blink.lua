20af9350e6b24b1e1eaddfacf61b562a49ad8f62
imtarcrow <github@tarcrow.com>
2026-05-06 00:09:13 +0200

    changed blink.cmp configuration

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,33 +1,33 @@
 return {
-	{
-		"saghen/blink.cmp",
-		lazy = false,
-		dependencies = { "rafamadriz/friendly-snippets" },
-		version = "1.*",
-		opts = {
-			appearance = {
-				nerd_font_variant = "mono",
-			},
-			completion = {
-				documentation = {
-					auto_show = true,
-				},
-			},
-			sources = {
-				default = {
-					"lsp",
-					"path",
-					"snippets",
-					"buffer",
-				},
-			},
-			fuzzy = {
-				implementation = "prefer_rust_with_warning",
-			},
-			signature = { enabled = true, window = { show_documentation = false } },
-		},
-		opts_extend = {
-			"sources.default",
-		},
-	},
+    {
+        "saghen/blink.cmp",
+        lazy = true,
+        dependencies = { "rafamadriz/friendly-snippets" },
+        version = "1.*",
+        opts = {
+            appearance = {
+                nerd_font_variant = "mono",
+            },
+            -- completion = {
+            --     documentation = {
+            --         auto_show = true,
+            --     },
+            -- },
+            sources = {
+                default = {
+                    "lsp",
+                    "path",
+                    "snippets",
+                    "buffer",
+                },
+            },
+            fuzzy = {
+                implementation = "prefer_rust_with_warning",
+            },
+            -- signature = { enabled = true, window = { show_documentation = false } },
+        },
+        opts_extend = {
+            "sources.default",
+        },
+    },
 }
