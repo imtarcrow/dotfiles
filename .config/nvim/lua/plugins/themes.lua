9fa81e26d165f51f384975601e0ddf4428addf94
imtarcrow <github@tarcrow.com>
2026-05-28 15:53:39 +0200

    fixed theme

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -1,6 +1,6 @@
 return {
     {
-        'https://github.com/nyoom-engineering/oxocarbon.nvim',
+        'https://github.com/projekt0n/github-nvim-theme',
         lazy = false,
         priority = 1000,
     },
