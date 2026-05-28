95bf295ff5d12bc65f89c57be11bef4ca6e1172d
imtarcrow <github@tarcrow.com>
2026-05-28 15:53:53 +0200

    changed neovim keybinds to make it more easy

--- .config/nvim/lua/core/keymap.lua
+++ .config/nvim/lua/core/keymap.lua
@@ -6,20 +6,26 @@ vim.api.nvim_create_autocmd("LspAttach", {
     group = vim.api.nvim_create_augroup("LspMappings", { clear = true }),
     callback = function(args)
         local bufnr = args.buf
-
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr })
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = bufnr })
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = bufnr })
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = bufnr })
         vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr })
 
-        vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, { buffer = bufnr })
-        vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, { buffer = bufnr })
-        vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, { buffer = bufnr })
-        vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, { buffer = bufnr })
-        vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, { buffer = bufnr })
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = bufnr })
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr })
+        vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { buffer = bufnr })
 
-        vim.keymap.set("n", "<leader>F", vim.lsp.buf.format, { buffer = bufnr })
+        vim.keymap.set("n", "gl", vim.diagnostic.open_float, { buffer = bufnr })
+        -- vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { buffer = bufnr })
+        -- vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { buffer = bufnr })
 
+        vim.keymap.set("n", "<leader>ws", require("telescope.builtin").lsp_workspace_symbols, { buffer = bufnr })
+        vim.keymap.set("n", "<leader>ds", require("telescope.builtin").lsp_document_symbols, { buffer = bufnr })
+
+        -- inlay hints
         vim.keymap.set("n", "<leader>th", function()
             vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = 0 }))
-        end, { buffer = bufnr, desc = "[T]oggle Inlay [H]ints" })
+        end, { buffer = bufnr, desc = "Toggle Inlay Hints" })
     end,
 })
