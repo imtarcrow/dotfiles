7a5f0dafb93d41ba1940abfac2a419d49d23dd77
imtarcrow <github@tarcrow.com>
2026-05-28 15:54:09 +0200

    changed rust lsp server settings

--- .config/nvim/lua/lsp/rust.lua
+++ .config/nvim/lua/lsp/rust.lua
@@ -1,16 +1,64 @@
 vim.lsp.config("rust_analyzer", {
     settings = {
         ["rust-analyzer"] = {
-            diagnostics = { enable = true },
+            check = {
+                command = "clippy",
+            },
+
+            diagnostics = {
+                enable = true,
+            },
+
+            procMacro = {
+                enable = true,
+            },
+
+            cargo = {
+                buildScripts = { enable = true },
+                features = "all",
+            },
+
             inlayHints = {
                 enable = true,
-                typeHints = true,
-                parameterHints = true,
-                chainingHints = true,
-                lifetimeElisionHints = {
+                typeHints = {
                     enable = true,
+                    hideClosureInitialization = false,
+                    hideNamedConstructor = false,
+                },
+                parameterHints = { enable = true },
+                chainingHints = { enable = true },
+                closureReturnTypeHints = { enable = "always" },
+                lifetimeElisionHints = {
+                    enable = "always",
                     useParameterNames = true,
                 },
+                bindingModeHints = { enable = true },
+                discriminantHints = { enable = "always" },
+                rangeExclusiveHints = { enable = true },
+            },
+
+            completion = {
+                autoimport = { enable = true },
+                autoself = { enable = true },
+                postfix = { enable = true },
+            },
+
+            imports = {
+                granularity = { group = "module" },
+                prefix = "self",
+            },
+
+            hover = {
+                actions = {
+                    enable = true,
+                    run = { enable = true },
+                    debug = { enable = true },
+                    gotoTypeDef = { enable = true },
+                    implementations = { enable = true },
+                    references = { enable = true },
+                },
+                documentation = { enable = true },
+                links = { enable = true },
             },
         },
     },
