7801ce9d75cdfcc2c1c252916627296b06ed561b
imtarcrow <github@tarcrow.com>
2026-05-31 21:54:49 +0200

    switched order of lazy.nvim being loaded and disabled providers

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,16 +1,10 @@
-vim.g.loaded_matchit = 1
-vim.g.loaded_matchparen = 1
-
-vim.g.mapleader = " "
-vim.g.localmapleader = "\\"
-
 Theme = "catppuccin"
-
--- load the lazy.nvim plugin manager
-require("core.lazy")
 
 -- set all options and keybinds
 require("core.options")
 require("core.keymap")
+
+-- load the lazy.nvim plugin manager
+require("core.lazy")
 
 vim.cmd("colorscheme " .. Theme)
--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,5 +1,10 @@
 require("core.autocommands")
 
+vim.g.mapleader = " "
+vim.g.localmapleader = "\\"
+
+
+
 -- Line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -17,7 +22,7 @@ vim.opt.incsearch = true
 
 -- UI
 vim.opt.showmode = false
-vim.opt.winborder = "single"
+-- vim.opt.winborder = "single"
 vim.opt.signcolumn = "yes"
 vim.opt.splitright = true
 vim.opt.splitbelow = true
@@ -33,5 +38,14 @@ vim.opt.swapfile = false
 vim.opt.undofile = true
 vim.opt.undodir = vim.fn.stdpath("cache") .. "/undo"
 
+-- disable neovim providers
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+
+-- Misc
+vim.g.loaded_matchit = 1
+vim.g.loaded_matchparen = 1
 vim.opt.mouse = ""
 vim.opt.clipboard = "unnamedplus"
